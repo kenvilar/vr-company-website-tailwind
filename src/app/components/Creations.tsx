09c93ab38d1e0ea2a67d44e109a58bfa87f039ff
kenvilar <kenvilar@gmail.com>
2023-05-06 23:49:14 +0800

    directives

--- src/app/components/Creations.tsx
+++ src/app/components/Creations.tsx
@@ -45,14 +45,11 @@ const Creations = () => {
           </div>
 
           {/*Items Container*/}
-          <div className="flex flex-col justify-between w-full space-y-6 text-2xl text-white uppercase md:flex-row md:space-y-0 md:space-x-8">
+          <div className="item-container">
             {/*item*/}
             {items.map((item, idx) => {
               return (
-                <div
-                  key={idx}
-                  className="group relative overflow-hidden md:w-1/4"
-                >
+                <div key={idx} className="group item">
                   {/*desktop*/}
                   <img
                     src={item.desktop}
@@ -66,10 +63,8 @@ const Creations = () => {
                     className="w-full md:hidden"
                   />
                   {/*gradient*/}
-                  <div className="absolute top-0 bottom-0 right-0 left-0 bg-gradient-to-b from-transparent to-gray-900 group-hover:from-gray-50 group-hover:to-white group-hover:opacity-70" />
-                  <h5 className="absolute px-6 duration-200 w-52 bottom-4 md:bottom-8 md:px-10 group-hover:scale-110 group-hover:text-black">
-                    {item.title}
-                  </h5>
+                  <div className="item-gradient" />
+                  <h5>{item.title}</h5>
                 </div>
               );
             })}
