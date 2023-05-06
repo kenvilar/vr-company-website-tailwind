95d253a89a295f16c1478c4b03b31b7bfd2ba79b
kenvilar <kenvilar@gmail.com>
2023-05-07 00:04:22 +0800

    implement Footer section

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,33 @@
+import Image from "next/image";
+import Logo from "@/app/assets/images/logo.svg";
+
 const Footer = () => {
+  const menuItems = ["About", "Careers", "Events", "Products", "Support"];
+
   return (
     <>
-      <div>Footer</div>
+      <footer className="bg-black">
+        <div className="container max-w-6xl py-10 mx-auto">
+          <div className="flex flex-col items-center mb-8 space-y-6 md:flex-row md:space-y-0 md:justify-between md:items-start">
+            <div className="flex flex-col items-center space-y-8 md:items-center md:space-y-4">
+              <div className="h-8">
+                <Image src={Logo} alt="logo" className="w-44 md:ml-3" />
+              </div>
+
+              <div className="flex flex-col items-center space-y-4 font-bold text-white md:flex-row md:space-y-0 md:space-x-6 md:ml-3">
+                {menuItems.map((item, idx) => {
+                  return (
+                    <div key={idx} className="h-10 group">
+                      <a href="#">{item}</a>
+                      <div className="mx-2 group-hover:border-b group-hover:border-blue-50" />
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
