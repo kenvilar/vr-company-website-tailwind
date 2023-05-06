bdc2cd90141fa276e034a8cad4278787b0f1d54c
kenvilar <kenvilar@gmail.com>
2023-05-06 20:49:37 +0800

    implement Feature section

--- src/app/components/Feature.tsx
+++ src/app/components/Feature.tsx
@@ -1,7 +1,32 @@
+import InterActive from "@/app/assets/images/desktop/image-interactive.jpg";
+import { Alata, Josefin_Sans } from "next/font/google";
+
+const alata = Alata({ weight: ["400"], subsets: ["latin"] });
+const josefin_sans = Josefin_Sans({ weight: ["300"], subsets: ["latin"] });
+
 const Feature = () => {
   return (
     <>
-      <div>Feature</div>
+      <section id="feature">
+        <div className="relative container flex flex-col max-w-6xl mx-auto my-32 px-6 text-gray-900 md:flex-row md:px-0">
+          <img src={InterActive.src} alt="interactive" />
+
+          <div className="top-48 pr-0 bg-white md:absolute md:right-0 md:py-20 md:pl-20">
+            <h2
+              className={`max-w-lg mt-10 mb-6 ${josefin_sans.className} text-4xl text-center text-gray-900 uppercase md:text-5xl md:mt-0 md:text-left`}
+            >
+              The leader in interactive VR
+            </h2>
+
+            <p className="max-w-md text-center md:text-left">
+              Founded in 2011, Loopstudios has been producing world-class
+              virtual reality projects for some of the best companies around the
+              globe. Our award-winning creations have transformed businesses
+              through digital experiences that bind to their brand.
+            </p>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
