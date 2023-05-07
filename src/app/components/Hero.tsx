fad89b66bc3da75935a2ca63264de5a0b7883e65
kenvilar <kenvilar@gmail.com>
2023-05-07 09:06:54 +0800

    minor change

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -45,7 +45,7 @@ const Hero = () => {
           {/*todo mobile menu*/}
           <div
             id="menu"
-            className="absolute top-0 bottom-0 left-0 flex-col self-end  w-full min-h-screen py-1 pt-40 pl-12 space-y-3 text-lg text-white uppercase bg-black"
+            className="absolute top-0 bottom-0 left-0 flex-col self-end hidden w-full min-h-screen py-1 pt-40 pl-12 space-y-3 text-lg text-white uppercase bg-black"
           >
             {menus.map((item, idx) => {
               return (
